f93ac49904b884c3a2302b3e8a61702a80c49de2
Ajay Singh Bisht <123948333+AJAYSINGHBISHT12@users.noreply.github.com>
2024-09-24 19:56:52 +0530

    Add files via upload

---
+++ data.py
@@ -0,0 +1,123 @@
+question_data = [
+    {
+        "response_code": 0,
+        "results": [
+            {
+                "type": "multiple",
+                "difficulty": "easy",
+                "category": "History",
+                "question": "In what year was the M1911 pistol designed?",
+                "correct_answer": "1911",
+                "incorrect_answers": [
+                    "1907",
+                    "1899",
+                    "1917"
+                ]
+            },
+            {
+                "type": "multiple",
+                "difficulty": "easy",
+                "category": "History",
+                "question": "What year did World War I begin?",
+                "correct_answer": "1914",
+                "incorrect_answers": [
+                    "1905",
+                    "1919",
+                    "1925"
+                ]
+            },
+            {
+                "type": "boolean",
+                "difficulty": "easy",
+                "category": "History",
+                "question": "The French Kingdom helped the United States gain their independence over Great Britain during the Revolutionary War.",
+                "correct_answer": "True",
+                "incorrect_answers": [
+                    "False"
+                ]
+            },
+            {
+                "type": "multiple",
+                "difficulty": "easy",
+                "category": "History",
+                "question": "Which famous world leader is famed for the saying, &quot;Let them eat cake&quot;, yet is rumored that he/she never said it at all?",
+                "correct_answer": "Marie Antoinette",
+                "incorrect_answers": [
+                    "Czar Nicholas II",
+                    "Elizabeth I",
+                    "Henry VIII"
+                ]
+            },
+            {
+                "type": "boolean",
+                "difficulty": "easy",
+                "category": "History",
+                "question": "The Tiananmen Square protests of 1989 were held in Hong Kong.",
+                "correct_answer": "False",
+                "incorrect_answers": [
+                    "True"
+                ]
+            },
+            {
+                "type": "multiple",
+                "difficulty": "easy",
+                "category": "History",
+                "question": "What was Manfred von Richthofen&#039;s nickname?",
+                "correct_answer": "The Red Baron",
+                "incorrect_answers": [
+                    "The High Flying Ace",
+                    "The Blue Serpent ",
+                    "The Germany Gunner"
+                ]
+            },
+            {
+                "type": "multiple",
+                "difficulty": "easy",
+                "category": "History",
+                "question": "Abolitionist John Brown raided the arsenal in which Virginia Town?",
+                "correct_answer": "Harper&#039;s Ferry",
+                "incorrect_answers": [
+                    "Richmond",
+                    "Harrisonburg",
+                    "Martinsburg"
+                ]
+            },
+            {
+                "type": "multiple",
+                "difficulty": "easy",
+                "category": "History",
+                "question": "In 1720, England was in massive debt and became involved in the South Sea Bubble. Who was the main mastermind behind it?",
+                "correct_answer": "John Blunt",
+                "incorrect_answers": [
+                    "Daniel Defoe",
+                    "Robert Harley",
+                    "John Churchill"
+                ]
+            },
+            {
+                "type": "multiple",
+                "difficulty": "easy",
+                "category": "History",
+                "question": "The original Roman alphabet lacked the following letters EXCEPT:",
+                "correct_answer": "X",
+                "incorrect_answers": [
+                    "W",
+                    "U",
+                    "J"
+                ]
+            },
+            {
+                "type": "multiple",
+                "difficulty": "easy",
+                "category": "History",
+                "question": "Who was the Prime Minister of Japan when Japan declared war on the US?",
+                "correct_answer": "Hideki Tojo",
+                "incorrect_answers": [
+                    "Michinomiya Hirohito",
+                    "Isoroku Yamamoto",
+                    "Fumimaro Konoe"
+                ]
+            }
+        ]
+    }
+]
