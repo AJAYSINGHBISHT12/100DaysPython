11c3d35e6af75cc6cc58e9bc2d573a2737314210
Ajay Singh Bisht <123948333+AJAYSINGHBISHT12@users.noreply.github.com>
2024-09-25 12:13:01 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
-from question_model import Question
-from data import question_data
-from quiz_brain import QuizBrain
+# import colorgram
+import turtle as turtle_module
+import random
 
-question_bank = []
-for question in question_data:
-    question_text = question["text"]
-    question_answer = question["answer"]
-    new_question = Question(question_text, question_answer)
-    question_bank.append(new_question)
+turtle_module.colormode(255)
+tim = turtle_module.Turtle()
+tim.speed("fastest")
+tim.penup()
+tim.hideturtle()
+# Extract 6 colors from an image.
+color_list = [(249, 248, 247), (239, 246, 244), (236, 242, 246), (245, 239, 243), (2, 13, 31), (217, 128, 107)]
+# colors = colorgram.extract('paint.png', 6)
+# for color in colors:
+#     r = color.rgb.r
+#     g = color.rgb.g
+#     b = color.rgb.b
+#     new_color = (r,g,b)
+#     rgb_colors.append(new_color)
+# print(rgb_colors)
+tim.setheading(225)
+tim.forward(300)
+tim.setheading(0)
+number_of_dots = 100
+for dot_count in range(1,number_of_dots + 1):
+    tim.dot(20, random.choice(color_list))
+    tim.forward(50)
+    if dot_count % 10 == 0:
+        tim.setheading(90)
+        tim.forward(50)
+        tim.setheading(180)
+        tim.forward(500)
+        tim.setheading(0)
 
-quiz = QuizBrain(question_bank)
-while quiz.still_has_questions():
-    quiz.next_question()
-print("You have completed the quiz.")
-print(f"your final score was: {quiz.score}/{quiz.question_number}")
+screen = turtle_module.Screen()
+screen.exitonclick()
